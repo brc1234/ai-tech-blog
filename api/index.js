75a1a61e00a359e0abee8ae164d2869e9628416e
BurcuAcir <burcuacir6@gmail.com>
2026-06-03 18:35:23 +0200

    MONGO_URI ismi guncellendi ve baglanti duzeltildi

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log('MongoDB Atlas bağlantısı başarılı!'))
     .catch(err => console.error('Bağlantı hatası:', err));
 
