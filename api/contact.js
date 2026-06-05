f58ef616d9f84fe3fdadab1391687f53c3d59e95
BurcuAcir <burcuacir6@gmail.com>
2026-06-05 19:27:36 +0200

    Add individual API route files for Vercel

---
+++ api/contact.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Message = require('../models/Message.js');
+
+module.exports = async (req, res) => {
+  try {
+    if (!process.env.MONGO_URL) {
+      return res.status(500).json({ error: 'MONGO_URL not set', success: false });
+    }
+
+    if (mongoose.connection.readyState === 0) {
+      await mongoose.connect(process.env.MONGO_URL);
+    }
+
+    if (req.method === 'POST') {
+      const newMessage = new Message(req.body);
+      const savedMessage = await newMessage.save();
+      res.status(201).json({ success: true, data: savedMessage });
+    } else {
+      res.status(405).json({ error: 'Method not allowed', success: false });
+    }
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+  }
+};
---
+++ api/posts.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Post = require('../models/Post.js');
+
+module.exports = async (req, res) => {
+  try {
+    if (!process.env.MONGO_URL) {
+      return res.status(500).json({ error: 'MONGO_URL not set' });
+    }
+
+    if (mongoose.connection.readyState === 0) {
+      await mongoose.connect(process.env.MONGO_URL);
+    }
+
+    if (req.method === 'GET') {
+      const posts = await Post.find().sort({ createdAt: -1 });
+      res.json(posts);
+    } else if (req.method === 'POST') {
+      const newPost = new Post(req.body);
+      const savedPost = await newPost.save();
+      res.status(201).json(savedPost);
+    } else {
+      res.status(405).json({ error: 'Method not allowed' });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
